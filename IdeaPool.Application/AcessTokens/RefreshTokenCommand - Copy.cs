8db3792799aed59157ef7dc5339061ee44877504
Long Le <lelong37@gmail.com>
2019-03-11 14:32:52 -0600

    Adding RefreshTokenCommand, RefreshTokenCommandHandler.

---
+++ IdeaPool.Application/AcessTokens/RefreshTokenCommand - Copy.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using IdeaPool.Application.Users.Models;
+using IdeaPool.Domain.Models;
+using MediatR;
+
+#endregion
+
+namespace IdeaPool.Application.Users.Commands
+{
+    public class DeleteTokenCommand: IRequest<Refresh>  
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
